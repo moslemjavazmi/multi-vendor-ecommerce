302d76d16675071f04335d3d7e7691e0726c4a8d
moslemjavazmi <moslemjavazmi@gmail.com>
2025-05-11 17:39:13 +0330

    set token with jwt

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,6 +1,7 @@
 const adminModel = require("../models/adminModel");
-const { responseReturn } = require("../utiles/response");
 const bcrypt = require("bcrypt");
+const { responseReturn } = require("../utiles/response");
+const { createToken } = require("../utiles/createToken");
 class authControllers {
   admin_login = async (req, res) => {
     const { email, password } = req.body;
@@ -9,8 +10,18 @@ class authControllers {
         .findOne({ email: email })
         .select("+password");
       if (admin) {
-        const match = bcrypt.compare(password, admin.password);
-        console.log("true", match);
+        const match = await bcrypt.compare(password, admin.password);
+        if (match) {
+          const token = await createToken({
+            id: admin.id,
+            role: admin.role,
+          });
+          res.cookie("accessToken", token, {
+            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+          });
+          responseReturn(res, 200, { token, message: "ورود موقیت آمیز بود" });
+        } else {
+        }
       } else {
         responseReturn(res, 400, { error: "ایمیل یافت نشد" });
       }
