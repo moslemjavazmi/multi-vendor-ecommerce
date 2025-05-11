c6ec3804abc5575a65b6177789459d5f10dffb64
moslemjavazmi <moslemjavazmi@gmail.com>
2025-05-11 17:45:41 +0330

    findOne admin email

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,5 +1,6 @@
 const adminModel = require("../models/adminModel");
 const { responseReturn } = require("../utiles/response");
+const bcrypt = require("bcrypt");
 class authControllers {
   admin_login = async (req, res) => {
     const { email, password } = req.body;
@@ -8,10 +9,12 @@ class authControllers {
         .findOne({ email: email })
         .select("+password");
       if (admin) {
+        const match = bcrypt.compare(password, admin.password);
+        console.log("true", match);
       } else {
         responseReturn(res, 400, { error: "ایمیل یافت نشد" });
       }
-      console.log(admin);
+      // console.log(admin);
     } catch (error) {
       responseReturn(res, 500, { error: error.messages });
     }
